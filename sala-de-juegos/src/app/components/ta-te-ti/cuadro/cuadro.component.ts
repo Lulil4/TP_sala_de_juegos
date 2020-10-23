191375172afc04f8aeb31471525fbf0e119b3249
lu <asd>
2020-10-23 00:44:09 -0300

    ta te ti vs pc en vez de j2

--- sala-de-juegos/src/app/components/ta-te-ti/cuadro/cuadro.component.ts
+++ sala-de-juegos/src/app/components/ta-te-ti/cuadro/cuadro.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
   <button [disabled]="bloqueado" mat-raised-button color="accent" *ngIf="value == '1'">♥</button>
   <button [disabled]="bloqueado" mat-raised-button color="accent" *ngIf="value == '2'">★</button>
 `,
-styles: ['button { width: 100%; height: 100%; font-size: 10vw !important; }']
+styles: ['button { width: 100%; height: 100%; font-size: 10vw !important; } @media (max-width: 500px) { button{padding-left:1px;}}']
 })
 export class CuadroComponent {
   
--- sala-de-juegos/src/app/components/ta-te-ti/ta-te-ti.component.ts
+++ sala-de-juegos/src/app/components/ta-te-ti/ta-te-ti.component.ts
@@ -11,6 +11,9 @@ export class TaTeTiComponent implements OnInit {
   turnoUno: boolean;
   ganador: string;
   bloqueoTurnos : boolean = false; 
+  cont : number = 0; 
+  primerTurno : boolean = true;
+  bloqueoNuevoJuego : boolean = true;
 
   constructor() {}
 
@@ -23,46 +26,81 @@ export class TaTeTiComponent implements OnInit {
     this.ganador = null;
     this.turnoUno = true;
     this.bloqueoTurnos = false;
+    this.cont = 0;
+    this.primerTurno = true;
+    this.bloqueoNuevoJuego = true;
   }
 
   get jugador() {
     return this.turnoUno ? '1' : '2';
   }
 
-  turnoUsuario(iCuadro: number) {
-    if (!this.cuadros[iCuadro]) {
-      this.cuadros.splice(iCuadro, 1, this.jugador);
-      this.turnoUno = !this.turnoUno;
-    }
+  turnoPC(){
+    let rand = null;
+    let cuadroElegido = null;
 
-    this.ganador = this.verResultado();
-
-    if (this.ganador != null){
-      this.bloqueoTurnos = true;
+    for (let i = 0; i<1000; i++){
+      rand = Math.random() * (9 - 1) + 1;
+      cuadroElegido = Math.floor(rand);
+      if (this.cuadros[cuadroElegido] == null){
+        this.cuadros[cuadroElegido] = "2"; 
+        break;
+      }
     }
   }
 
-  verResultado() {
-    let retorno = null;
-    const lineas = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lineas.length; i++) {
-      const [a, b, c] = lineas[i];
-      if (
-        this.cuadros[a] &&
-        this.cuadros[a] === this.cuadros[b] &&
-        this.cuadros[a] === this.cuadros[c]
-      ) {
-        retorno = this.cuadros[a];
+  turnoUsuario(iCuadro: number) {
+    if (this.turnoUno){
+      if (!this.cuadros[iCuadro]) {
+        this.bloqueoNuevoJuego = true;
+        this.bloqueoTurnos = true;
+        this.cuadros.splice(iCuadro, 1, this.jugador);
+        this.turnoUno = !this.turnoUno;
+        this.ganador = this.verResultado();
+        if (this.ganador != null){
+          this.bloqueoTurnos = true;
+          this.bloqueoNuevoJuego = false;
+        }
+        if (this.cont < 9 && this.ganador == null){
+          setTimeout(() => {
+            this.turnoPC();
+            this.turnoUno = !this.turnoUno;
+            this.bloqueoTurnos = false;
+            this.bloqueoNuevoJuego = false;
+            this.ganador = this.verResultado();
+            if (this.ganador != null){
+              this.bloqueoTurnos = true;
+              this.bloqueoNuevoJuego = false;
+            }
+          },2000); 
+        }
       }
+    }
+
+  }
+
+  verResultado() {
+    this.cont++;
+    let retorno = null;
+      const lineas = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+      for (let i = 0; i < lineas.length; i++) {
+        const [a, b, c] = lineas[i];
+        if (this.cuadros[a] && this.cuadros[a] === this.cuadros[b] && this.cuadros[a] === this.cuadros[c]) {
+          retorno = this.cuadros[a];
+        }
+      }
+    
+    if (this.cont == 9 && retorno == null){
+      retorno = "ninguno";
     }
     return retorno;
   }
